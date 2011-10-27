f0714a7062ecb01040864f1cb663bb39e22aa2fa
Adrian Unger <dev@staydecent.ca>
2011-10-27 10:46:39 -0700

    Cleaned up some variables and asking some questions.

--- theme_name/includes/custom_field_functions.php
+++ theme_name/includes/custom_field_functions.php
@@ -1,39 +1,56 @@
 <?php
 
 	/* Functions for processing custom fields */
-
-	function get_custom_field($field, $format = '', $date_format = 'F j, Y')
+	// just semantics, but is more clear.	
+	function format_custom_field($field, $format = '', $date_format = 'F j, Y')
 	{
-		$fetch = $field;
+		// No need to declar the var here, it's created below		
+		// $fetch = $field;
 		$fetch = get_post_custom_values($field);
 		$fetch = $fetch[0];
-
-		// Date
-		if ($format == 'date' & $fetch !='') $fetch = format_date($fetch, $date_format);
-			
-		// Text Block
-		elseif ($format == 'text_block') $fetch = wpautop($fetch);
-
-		// Link
-		elseif ($format == 'link') $fetch = '<a href="'.$fetch.'" class="custom_link">'.$fetch.'</a>';
-
-		// HTML
-		elseif ($format == 'html') $fetch = html_entity_decode($fetch);
-			
-		// Google Map
-		elseif ($format == 'google_map') $fetch = display_google_map($fetch);
-
-		return $fetch;
+		
+		// Test if anything was fetched or return false. Should test using empty()
+		if (!$fetch) {
+			return false;	
+		}
+		
+		// switches make more sense for testing the same variable multiple times
+		// also, as you're not running any other checks on the $fetch var, why
+		// not return the value right away?		
+		switch ($format) {
+			case 'date':
+				return date($date_format, $fetch);
+				break;
+			case 'text_block':
+				return wpautop($fetch);
+				break;
+			case 'link':
+				return '<a href="'.$fetch.'" class="custom_link">'.$fetch.'</a>';
+				break;
+			case 'html':
+				return html_entity_decode($fetch);
+				break;
+			case 'google_map':
+				return display_google_map($fetch);
+				break;
+			default:
+				// If you're not processing(formatting) the var at all
+				// why even use this function? Just use the WP custom field funcs.
+				return $fetch;
+				
+		}
 	}
 
 	// Get and echo custom field data
+	// Preference I suppose, but feels like cluttering the function namespace.
 	function custom_field($field, $format = '', $date_format = 'F j, Y')
 	{
-		echo get_custom_field($field, $format, $date_format);
+		echo format_custom_field($field, $format, $date_format);
 	}
 
 	
 	// Formatting for dates
+	// Why? This is a rare case where PHP's param ordering makes sense.
 	function format_date($date, $date_format)
 	{
 		$date = date($date_format, $date);
